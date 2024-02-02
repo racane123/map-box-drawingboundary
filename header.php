5fd519ae26cc9d1132f13375692786101b9a6eb9
racane123 <neograpic@gmail.com>
2024-02-02 18:29:40 +0800

    adding the dropdown of the users

--- header.php
+++ header.php
@@ -13,3 +13,4 @@
 <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css”/>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>    
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.0-beta2/js/bootstrap.bundle.min.js"></script>
--- navbar.php
+++ navbar.php
@@ -16,37 +16,54 @@
     font-weight: bold;
     font-style: normal;
   }
+ li{
+    color:black;
+  }
 </style>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="images/logo-text.png" alt="logo">TownTechInnovations</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mr-auto">
+<nav class="navbar navbar-expand-lg navbar-dark bg-blue">
+  <div class="container">
+    <!-- Logo -->
+    <a class="navbar-brand text-dark" href="#"><img src="images/logo-text.png" alt="logo">TownTechInnovations</a>
+    <!-- Toggle button for mobile view -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navigation links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
                 <?php
                 // Check if the user is an admin to display the Dashboard link
                 if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
-                    echo '<li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>';
+                    echo '<li class="nav-item"><a class="nav-link text-dark" href="dashboard.php">Dashboard</a></li>';
                 }
                 ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="#" onclick="showMap()">Map</a>
+                    <a class="nav-link text-dark" href="#" onclick="showMap()">Chloropleth</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#" onclick="showMap()">Map</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#" onclick="showMap()">Map</a>
                 </li>
 
                 <?php
                 if (isset($_SESSION['email'])) {
-                    echo '<li class="nav-item text-left"><a class="nav-link" href="#">'. $_SESSION["email"]. '</a></li>';
-                    echo '<li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>';
+                    echo '<li class="nav-item dropdown">';
+                    echo '<a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">' . $_SESSION["email"] . '</a>';
+                    echo '<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">';
+                    echo '<li><a class="dropdown-item text-dark" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>';
+                    echo '</ul>';
+                    echo '</li>';
                 }
                 ?>
-            </ul>
-        </div>
+      </ul>
     </div>
+  </div>
 </nav>
+
 
 
 
