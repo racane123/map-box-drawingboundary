3b8d4949439ca025bd8d2f465e66c6528782e882
racane123 <neograpic@gmail.com>
2024-02-13 21:14:27 +0800

    adding the session

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Interactive Map with Drawing</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  <!-- Bootstrap CDN  -->
-  
-  <!-- Map Box CDN-->
-  <link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet">
-  <script src="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.js"></script>
-  <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.js"></script>
-  <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
-  <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
-  <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.css" type="text/css" />
-  <?php
+<?php
   session_start();
- 
+
   include 'header.php';
   
-  ?>
-  
+?>
+
   <style>
     
     body { margin: 0; padding: 0; }
@@ -31,7 +16,7 @@
       position: absolute;
     }
   </style>
-</head>
+
 <body>
 <?php
 include "header.php";
@@ -428,5 +413,3 @@ function filterMarkers() {
 }
 </script>
 </body>
-
-</html>
