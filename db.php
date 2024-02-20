837f86b2076bab5f14b1eae2c2530545a3a9af9f
DelaPuz28 <jonathan.delapuz64@gmail.com>
2024-02-20 17:56:45 +0800

    database changes

--- db.php
+++ db.php
@@ -6,16 +6,23 @@ $db_name = 'if0_35435943_gis';
 $username = 'if0_35435943';
 $password = 'YETZXocfNE';
 $port = '3306';
-*/
+
 
 $server = 'localhost';
 $db_name = 'gis';
 $username = 'root';
 $password = '';
 $port = '3390';
+*/
+
+$server = 'localhost';
+$db_name = 'grou_gis';
+$username = 'root';
+$password = 'LrsLsl+MB4qdrz!e';
+
 
 // Create connection
-$dbconn = new mysqli($server, $username, $password, $db_name, $port);
+$dbconn = new mysqli($server, $username, $password, $db_name);
 
 // Check connection
 if ($dbconn->connect_error) {
