0a694fe298d9585790993494f40565f508aa7c32
racane123 <neograpic@gmail.com>
2024-02-20 14:23:02 +0800

    changing the db connection bla bla

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,10 +1,15 @@
 <?php
 // Connection parameters
 $host = 'localhost';
-$username = 'root'; 
-$password = ''; 
-$database = 'localgovernment';
-$port = '3390';
+$username = 'grou_racane123'; 
+$password = '045ExxP-3O3p4@xu'; 
+$database = 'grou_localgovernment';
+
+//$host = 'localhost';
+//$username = 'root'; 
+//$password = ''; 
+//$database = 'localgovernment';
+//$port = 3308;
 
 
 $conn = mysqli_connect($host, $username, $password, $database,$port);
