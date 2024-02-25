2ff9d45c4e6250b91813702e201728b9dbac6062
racane123 <neograpic@gmail.com>
2024-02-25 19:30:28 +0800

    updating database

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,17 +1,17 @@
 <?php
 // Connection parameters
-//$host = 'localhost';
-//$username = 'grou_racane123'; 
-//$password = '045ExxP-3O3p4@xu'; 
-//$database = 'grou_localgovernment';
+$host = 'localhost';
+$username = 'grou_racane123'; 
+$password = '045ExxP-3O3p4@xu'; 
+$database = 'grou_localgovernment';
+/*
+$host = 'localhost';
+$username = 'root'; 
+$password = ''; 
+$database = 'localgovernment';
+$port = 3308;
 
-//$host = 'localhost';
-//$username = 'root'; 
-//$password = ''; 
-//$database = 'localgovernment';
-//$port = 3390;
-
-
+*/
 $conn = mysqli_connect($host, $username, $password, $database,$port);
 
 
